25d7a4384d6f0eab5abff04e3a8eb9d798d557c0
leoren0971 <872106608@qq.com>
2019-05-17 15:15:53 +0800

    Site updated: 2019-05-17 15:15:53

---
+++ app/precache-manifest.1440defa41568234c1de8957fdfb4e68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41022d09e6e4fd2027a1f521203f666",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20495fabe32d4d2044c9",
+    "url": "/static/css/2.1ad4406c.chunk.css"
+  },
+  {
+    "revision": "ccc337d8ee04c8377f65",
+    "url": "/static/css/main.4c5808ae.chunk.css"
+  },
+  {
+    "revision": "20495fabe32d4d2044c9",
+    "url": "/static/js/2.569a586c.chunk.js"
+  },
+  {
+    "revision": "ccc337d8ee04c8377f65",
+    "url": "/static/js/main.03483dbf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
